be8c24653f5c12f0c2ccd0f1f87e98c75b24b0dc
Nathan Augustinov <nathan.augustinov@student.upt.ro>
2023-12-21 19:08:54 +0200

    Project setup

---
+++ src/main/java/com/programming/SportsEquipment/SportsEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.programming.SportsEquipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsEquipmentApplication.class, args);
+	}
+
+}
